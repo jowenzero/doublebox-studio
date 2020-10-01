6b765e0d845cc726da549dcb336502cbd59cf8cb
jowenzero <jowenzero@gmail.com>
2020-10-01 19:13:28 +0700

    update drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -4,6 +4,9 @@ import {
   Grid,
   Container,
   Drawer,
+  Typography,
+  ButtonBase,
+  Link
 } from "@material-ui/core";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Background from '../public/images/drawer-bg.svg';
@@ -14,10 +17,27 @@ import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Slider from "react-slick";
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+
+const games = [
+  {
+    name: "Run Cube Run",
+    image: "/images/rcr-logo.png",
+  },
+  {
+    name: "Reversible",
+    image: "/images/reversible-logo.png",
+  },
+  {
+    name: "Sorrow Path of Memoir",
+    image: "/images/spom-logo.png",
+  },
+];
 
 const Accordion = withStyles({
   root: {
-    borderBottom: '1px solid #000000',
+    borderBottom: '0px solid #fff',
     boxShadow: 'none',
     '&:before': {
       display: 'none',
@@ -31,11 +51,10 @@ const Accordion = withStyles({
 
 const AccordionSummary = withStyles({
   root: {
-    borderBottom: '1px solid #000000',
-    marginBottom: -1,
-    minHeight: 56,
+    borderBottom: '3px solid #fff',
+    height: 77,
     '&$expanded': {
-      minHeight: 56,
+      height: 77,
     },
   },
   content: {
@@ -48,7 +67,7 @@ const AccordionSummary = withStyles({
 
 const AccordionDetails = withStyles((theme) => ({
   root: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
   },
 }))(MuiAccordionDetails);
 
@@ -67,25 +86,27 @@ const useStyles = makeStyles(theme => ({
     paddingTop: 60,
   },
   rootExpand: {
-    width: "100%",
+    width: "96%",
     boxShadow: "none",
-    backgroundColor: '#252526',
-    overflow: 'hidden'
+    backgroundColor: 'transparent',
+    overflow: 'hidden',
   },
   summary: {
     width: "100%",
-    height: 30,
     boxShadow: "none",
-    backgroundColor: '#252526',
-    borderBottom: '1px solid #000000'
+    backgroundColor: 'transparent',
+    marginBottom: 20,
   },
   details: {
-    position: 'relative',
-    width: "110%",
-    left: '-5%',
+    width: "100%",
     boxShadow: "none",
-    backgroundColor: '#252526',
+    backgroundColor: 'transparent',
     overflow: 'hidden'
+  },
+  quoteWrap: {
+    overflow: "hidden",
+    marginLeft: 0,
+    marginRight: 0
   },
 }));
 
@@ -95,46 +116,226 @@ const DrawerComponent = (props) => {
 
   const SideList = () => (
     <Container maxWidth="xl">
-      <Grid container spacing={1} style={{ marginTop: 20, marginBottom: 50 }}>
+      <Grid
+        container
+        style={{ marginTop: 30, marginBottom: 50 }}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >  
         <div style={{ 
           color: 'transparent', 
           background: "#DC6624", 
-          width: "95%", 
+          width: "96%", 
           height: "9px", 
-          marginLeft: '2%',
+          marginBottom: 20,
           boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
-        }}>.</div>
-        <div style={{ 
-          color: 'transparent', 
-          background: "#C33795", 
-          width: "9px",
-          height: "30px",
-          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
-        }}>.</div>
+        }}>
+          <div style={{ 
+            color: 'transparent', 
+            background: "#C33795", 
+            marginLeft: "99.5%",
+            width: "9px",
+            height: "30px",
+            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            textAlign: 'right',
+          }}>.</div>
+        </div>
 
-        {/* <Accordion square className={classes.rootExpand}>
+        <div style={{
+          width: "96%",
+          borderBottom: '3px solid #fff',
+          height: 77,
+          marginBottom: 20,
+        }}>
+          <Link 
+            href="#" 
+          >
+            <Typography
+              gutterBottom
+              variant="body2"
+              style={{
+                color: "#fff", 
+                fontSize: 32, 
+                marginTop: 10,
+                marginLeft: 15,
+              }}
+            >
+              News
+            </Typography>
+          </Link>
+        </div>
+
+        <Accordion square className={classes.rootExpand}>
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon style={{ color: "#fff" }} />}
+            aria-label="Expand"
+            expandIcon={<ExpandMoreIcon style={{ color: "#fff", fontSize: 40 }} />}
             className={classes.summary}
           >
-            <p style={{ color: "#fff" }}>Screens</p>
+            <Link 
+              href="#" 
+              aria-label="Link"
+              onClick={(event) => event.stopPropagation()}
+              onFocus={(event) => event.stopPropagation()}
+            >
+              <Typography
+                gutterBottom
+                variant="body2"
+                style={{
+                  color: "#fff", 
+                  fontSize: 32, 
+                  marginBottom: 6,
+                }}
+              >
+                Games
+              </Typography>
+            </Link>
           </AccordionSummary>
           <AccordionDetails
             className={classes.details}
           >
-            <div
-              style={{
-                display: "flex",
-                flexWrap: 'wrap',
-                marginLeft: 5,
-                marginRight: 5,
-                width: '100%'
-              }}
-            >
-              <h1>hi</h1>
+            <div className={classes.quoteWrap}>
+              <ScrollMenu
+                arrowLeft={<div style={{ fontSize: "30px", marginRight: 10 }}>{" < "}</div>}
+                arrowRight={<div style={{ fontSize: "30px", marginLeft: 10 }}>{" > "}</div>}
+                itemStyle={{
+                  outline: 'none'
+                }}
+                wheel={false}
+                alignCenter={false}
+                data={games.map((item, index) => (
+                  <div style={{
+                    marginRight: 20,
+                    marginLeft: 20,
+                  }} key={index}>
+                    <ButtonBase>
+                      <div style={{ width: 215, height: 215 }}>
+                        { item.image ? 
+                          <>
+                            <img
+                              src={item.image}
+                              alt=""
+                              style={{
+                                  width: '100%',
+                                  height: '100%'
+                              }}
+                            />
+                            <Typography
+                              gutterBottom
+                              variant="body1"
+                              style={{
+                                position: 'absolute', 
+                                bottom: 8,
+                                marginLeft: 10,
+                                color: "#fff", 
+                                fontSize: 24,
+                                textAlign: 'left',
+                              }}
+                            >
+                              {item.name}
+                            </Typography>
+                          </> :
+                          <div style={{
+                            width: '100%',
+                            height: '100%',
+                            backgroundColor: '#303030',
+                          }}>
+                            <Typography
+                                gutterBottom
+                                variant="body1"
+                                style={{
+                                  position: 'absolute', 
+                                  bottom: 8,
+                                  marginLeft: 10,
+                                  color: "#fff", 
+                                  fontSize: 24,
+                                  textAlign: 'left',
+                                }}
+                              >
+                                {item.name}
+                            </Typography>
+                          </div>
+                        }
+                      </div>
+                    </ButtonBase>
+                  </div>
+                ))}
+              />
             </div>
           </AccordionDetails>
-        </Accordion> */}
+        </Accordion>
+
+        <div style={{
+          width: "96%",
+          borderBottom: '3px solid #fff',
+          height: 77,
+          marginBottom: 20,
+        }}>
+          <Link 
+            href="#" 
+          >
+            <Typography
+              gutterBottom
+              variant="body2"
+              style={{
+                color: "#fff", 
+                fontSize: 32, 
+                marginTop: 10,
+                marginLeft: 15,
+              }}
+            >
+              Blogs
+            </Typography>
+          </Link>
+        </div>
+
+        <div style={{
+          width: "96%",
+          borderBottom: '3px solid #fff',
+          height: 77,
+          marginBottom: 20,
+        }}>
+          <Link 
+            href="#" 
+          >
+            <Typography
+              gutterBottom
+              variant="body2"
+              style={{
+                color: "#fff", 
+                fontSize: 32, 
+                marginTop: 10,
+                marginLeft: 15,
+              }}
+            >
+              About Us
+            </Typography>
+          </Link>
+        </div>
+
+        <div style={{
+          width: "96%",
+          borderBottom: '3px solid #fff',
+          height: 77,
+          marginBottom: 20,
+        }}>
+          <Link 
+            href="#" 
+          >
+            <Typography
+              gutterBottom
+              variant="body2"
+              style={{
+                color: "#fff", 
+                fontSize: 32, 
+                marginTop: 10,
+                marginLeft: 15,
+              }}
+            >
+              Support
+            </Typography>
+          </Link>
+        </div>
       </Grid>
     </Container>
   );
--- components/Head.js
+++ components/Head.js
@@ -27,6 +27,17 @@ const HeadComponent = () => (
         {/* Custom */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
         {/* Favicon */}
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
